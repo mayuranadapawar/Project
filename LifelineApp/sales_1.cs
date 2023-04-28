62a61258dd5981cc250b36c69d1972beef02d0e9
mayur <mayurpawar4177@gmail.com>
2023-04-28 13:33:40 +0530

    28-4-23

---
+++ LifelineApp/sales_1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LifelineApp
+{
+    public partial class sales_1 : Form
+    {
+        public sales_1()
+        {
+            InitializeComponent();
+        }
+    }
+}
